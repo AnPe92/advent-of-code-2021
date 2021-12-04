f0ba148db8e60eca3ada3f8c4bd2c607d0b71cf4
AnPe92 <93088524+AnPe92@users.noreply.github.com>
2021-12-04 02:06:43 +0100

    Update part1.js

--- day2/part1.js
+++ day2/part1.js
@@ -1,1 +1,24 @@
+let input = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`.trim().split("\n");
 
+const objectOfOrders = {};
+let depth = 0;
+
+input.forEach(string => {
+   const [key, value] = string.split(" ");
+	 if(objectOfOrders[key]){
+   objectOfOrders[key] = objectOfOrders[key] += parseInt(value);
+	 } else {
+	 objectOfOrders[key] = parseInt(value);
+	 }
+});
+
+depth = depth + objectOfOrders.down - objectOfOrders.up
+
+let sum = objectOfOrders.forward * depth;
+
+console.log(sum);
